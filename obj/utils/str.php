4cce8a0b37cbfa4ee0fb0d8ef8605cc5cb7f95b8
Antti Stenvall <antti@stenvall.fi>
2015-08-30 20:46:03 +0300

    string modifier html2latex

--- obj/utils/str.php
+++ obj/utils/str.php
@@ -4,13 +4,25 @@ namespace mcc\obj\utils;
 
 class str {
 
-  static function normalize($str){
-    $replacers = array('ä' => 'a','ö' => 'o','\s' => '-');
+  static public function normalize($str) {
+    $replacers = array('ä' => 'a', 'ö' => 'o', '\s' => '-');
     $str = strtolower($str);
-    foreach ($replacers as $key => $value){
-      $str = preg_replace('#' . $key . '#',$value,$str);
-    }    
+    foreach ($replacers as $key => $value) {
+      $str = preg_replace('#' . $key . '#', $value, $str);
+    }
     return $str;
-  }    
+  }
+
+  // convert subscripts to latex subscripts, italic to italics etc
+  static public function html2latex($str) {
+    $replacers = array(
+        '<sub>([0-9]*)</sub>' => '$_{$1}$',
+        '<sub>(.*)</sub>' => '$_{\text{$1}}$',
+        '<i>(.*)</i>' => '$$1$');
+    foreach ($replacers as $key => $value) {
+      $str = preg_replace('#' . $key . '#', $value, $str);
+    }
+    return $str;
+  }
 
 }
